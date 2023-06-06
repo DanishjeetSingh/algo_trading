4204a5c6d97b58293178792db7cf02e1be828ce1
DanishjeetSingh <danishjeetsingh@gmail.com>
2023-06-06 01:21:11 -0400

    added more comments

--- gpt.py
+++ gpt.py
@@ -24,6 +24,14 @@ openai.api_key = config['OPENAI']['api_key']
 response = openai.Completion.create(
   model="text-davinci-003",
   prompt="f{tweets} Jim Cramer recommends selling the following stock tickers: ",
+  max_tokens=32, # max tokens to generate
+
+  # either change temp or top_p not both
+  temperature=0.7, # b/w 0 and 2, 0.7 will make it more random, 0.2 will make it more deterministic
+  # 1 is default for top_p
+  top_p=1, # to select the top tokens with a probability 1.0
+  frequency_penalty=0, # a +ve val decreases model likelihood to repeat stuff
+  presence_penalty=0, # a +ve val increases model likelihood to talk about new stuff
 )
 
 # the regex looks for the words with uppercase letters
