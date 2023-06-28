f1ce2cb777eb1c714431d4f40e0b99752866620f
DanishjeetSingh <danishjeetsingh@gmail.com>
2023-06-27 22:39:33 -0400

    tidy the code up

--- trading.py
+++ trading.py
@@ -15,28 +15,22 @@ config.read('config.ini')
 
 trading_client = TradingClient(api_key=config['ALPACA']['api_key'], secret_key=config['ALPACA']['secret_key'], paper=True)
 
-# account = trading_client.get_account()
-# for property_name, value in account:
-#   print(f"\"{property_name}\": {value}")
+buy_stocks = get_stocks(api_key=config['OPENAI']['api_key'], bearer_token = config['TWITTER_CREDS']['bearer_token'])
 
-buy_stocks = get_stocks(api_key=config['OPENAI']['api_key'])
-# print(buy_stocks)
 liquidate = trading_client.close_all_positions(cancel_orders=True)
-print(liquidate)
+# print(liquidate)
+
 # Setting parameters for our buy order
 market_order_data = MarketOrderRequest(
                       symbol=buy_stocks[0],
                       qty=1,
                       side=OrderSide.BUY,
-                      time_in_force=TimeInForce.UTC
+                      time_in_force=TimeInForce.DAY
                   )
-print(f'currently buying {buy_stocks[0]}')
+# print(f'currently buying {buy_stocks[0]}')
 # cancel and liquidate and all the orders
 
 
 # Submitting the order and then printing the returned object
 market_order = trading_client.submit_order(market_order_data)
 
-# print(len(market_order))
-# for property_name, value in market_order:
-  # print(f"\"{property_name}\": {value}")
