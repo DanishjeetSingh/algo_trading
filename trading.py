4ed199b586e6f6e416553e43218fe61e112bb55e
DanishjeetSingh <danishjeetsingh@gmail.com>
2023-06-29 00:16:13 -0400

    added notional

--- trading.py
+++ trading.py
@@ -9,7 +9,7 @@ from configparser import ConfigParser
 from alpaca.trading.client import TradingClient
 from gpt import get_stocks
 from alpaca.trading.requests import MarketOrderRequest
-from alpaca.trading.enums import OrderSide, TimeInForce
+from alpaca.trading.enums import OrderSide, TimeInForce, OrderType
 
 logging.basicConfig(filename='logfile.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
@@ -18,8 +18,10 @@ config = ConfigParser(interpolation=None)
 config.read('config.ini')
 
 trading_client = TradingClient(api_key=config['ALPACA']['api_key'], secret_key=config['ALPACA']['secret_key'], paper=True)
+account = trading_client.get_account()
 
 buy_stocks = get_stocks(api_key=config['OPENAI']['api_key'], bearer_token = config['TWITTER_CREDS']['bearer_token'])
+# print(buy_stocks)
 
 liquidate = trading_client.close_all_positions(cancel_orders=True)
 # print(liquidate)
@@ -34,8 +36,10 @@ if not buy_stocks:
 else:
     market_order_data = MarketOrderRequest(
                           symbol=buy_stocks[0],
-                          qty=1,
+                          # qty=1,
+                          notional= int(float(account.buying_power) * 0.9),
                           side=OrderSide.BUY,
+                          type= OrderType.MARKET,
                           time_in_force=TimeInForce.DAY
                       )
     logging.info(f'Buying {buy_stocks[0]}')
