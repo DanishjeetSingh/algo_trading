c43683f265214e73a8648f880e11875672f555ce
DanishjeetSingh <danishjeetsingh@gmail.com>
2023-07-11 11:10:48 -0400

    added docstring and todo

---
+++ scrape.py
@@ -0,0 +1,7 @@
+"""
+Purpose:
+    This script will be used to log into twitter and then scrape tweets for a given user on Twitter
+"""
+ # TODO: Selenium headless browser should be able to handle twitter login?
+ #       and Beautiful Soup for HTML parsing?
+
